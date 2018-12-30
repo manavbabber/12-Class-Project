4e695115d9cc4c1630e1941af02baa35fda80aff
Manavbabber <manavfazilka@gmail.com>
2018-12-30 08:05:18 +0000

    Upload New File

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<fstream>
+#include<string>             //--->   Libraries Used
+#include<stdio.h>
+#include"login.h"
+#include"hotel.h"
+
+using namespace std ;
+
+int main()
+{
+  mainmenu();
+  Show();
+  drone();
+}
+
+
+
+
+
+
+
